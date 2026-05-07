f0e3c47ed1686c9867693dc89e18280628da5c42
Yadunand Pavithran <118120111+hi-yadunand@users.noreply.github.com>
2026-05-07 15:03:24 +0530

    Revert "update"
    This reverts commit ba2d4daf1d97dd8a834484305b1bd6b39f11da8c.

--- app/(site)/faq/page.js
+++ app/(site)/faq/page.js
@@ -143,7 +143,7 @@ const pageHtml = `<div id="preloader">
   </header>
 
 
-  <div id="has_smooth"></div>
+  <div class="has-smooth" id="has_smooth"></div>
   <div id="smooth-wrapper">
     <div id="smooth-content">
       <div class="body-wrapper body-corporate-agency">
@@ -230,7 +230,7 @@ export default function FaqPage() {
   return (
     <>
       <link rel="stylesheet" href="/assets/css/master-faq.css" />
-      <link rel="stylesheet" href="/assets/css/contact-overrides.css?v=4" />
+      <link rel="stylesheet" href="/assets/css/contact-overrides.css?v=3" />
       <div
         className={`public-arolax-page font-heading-beatricetrial-regular-2`}
         dangerouslySetInnerHTML={{ __html: pageHtml }}
